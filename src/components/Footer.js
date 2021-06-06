2665966f635bde94f2a01c172ac3425e3a376210
latifaghassan <lalooshamayra@hotmail.com>
2021-06-06 22:52:37 +0300

    finished

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react"; // import the react class from the react liberary
+
+// Define a class component named Footer
+// That inherits thr Class
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <title>Unicorn Head</title>
+        <h2>description</h2>
+        <p>Someone wearing a creepy unicorn head mask</p>
+        <h2>keyword</h2>
+        <p>unicorn</p>
+        <h2>horns</h2>
+        <p>3</p>
+        <img
+          src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+          alt="Rhino Family"
+        />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from "react"; // import the react class from the react liberary
+
+// Define a class component named Footer
+// That inherits thr Class
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <title>Rhino Family</title>
+        <h2>description</h2>
+        <p>Mother (or father) rhino with two babies</p>
+        <h2>latifa</h2>
+        <p>rhino</p>
+        <h2>horns</h2>
+        <p>2</p>
+        <img
+          src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+          alt="Rhino Family"
+        />
+      </main>
+    );
+  }
+}
+
+export default Main;
+
+// when you want to add img {imageURL}
