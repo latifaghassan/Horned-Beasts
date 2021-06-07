86d7aef4eb5dc4846babf16e3734570132b01115
latifaghassan <lalooshamayra@hotmail.com>
2021-06-07 21:07:58 +0300

    finished

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,29 +1,31 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0,
+    };
+  }
+
+  votingFunc = () => {
+    this.setState({
+      votes: this.state.votes + 1, // ++ += +1
+    });
+  };
   render() {
     return (
       <div>
-        <h2>UniWhal</h2>
-        <img
-          src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          alt="1"
-        />
-        <p>A unicorn and a narwhal nuzzling their horns</p>
+        <title>{this.props.title}</title>
+        <h2>{this.props.description}</h2>
 
-        <h2>Rhino Family</h2>
         <img
-          src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-          alt="2"
+          onClick={this.votingFunc}
+          src={this.props.url}
+          alt={this.props.title}
         />
-        <p>Mother (or father) rhino with two babies</p>
 
-        <h2>Unicorn Head</h2>
-        <img
-          src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-          alt=""
-        />
-        <p>Someone wearing a creepy unicorn head mask</p>
+        <p>{this.state.votes}</p>
       </div>
     );
   }
@@ -31,3 +33,19 @@ class HornedBeast extends React.Component {
 
 export default HornedBeast;
 /*********************************/
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
